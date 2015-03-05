de0cafec261e51fe19c73b95dd74bc63b94e7585
Brit Butler <redline6561@gmail.com>
2015-03-05 11:58:09 -0500

    Add default layout and fix from address.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@serene-sea-7663@herokuapp.com'
+  default from: 'no-reply@serene-sea-7663.herokuapp.com'
+  layout 'default'
 end
